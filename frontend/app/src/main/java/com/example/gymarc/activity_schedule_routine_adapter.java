04ee2f58e873f24d4f8c2489b6dd0df642af7eee
diego coira <diegocoira@gmail.com>
2024-06-03 18:40:27 +0200

    Bottom menu added, changed activity_workout -> activity_activity_schedule_routine

--- frontend/app/src/main/java/com/example/gymarc/activity_schedule_routine_adapter.java
+++ frontend/app/src/main/java/com/example/gymarc/activity_schedule_routine_adapter.java
@@ -8,14 +8,13 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.gymarc.model.RoutineGroup;
-import com.example.gymarc.model.WeeklyRoutineDay;
 
 import java.util.List;
 
-public class activity_workout_adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class activity_schedule_routine_adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<RoutineGroup> routineGroups;
 
-    public activity_workout_adapter(List<RoutineGroup> routineGroups) {
+    public activity_schedule_routine_adapter(List<RoutineGroup> routineGroups) {
         this.routineGroups = routineGroups;
     }
 
@@ -28,14 +27,14 @@ public class activity_workout_adapter extends RecyclerView.Adapter<RecyclerView.
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View cell_view = inflater.inflate(R.layout.activity_workout_cell, parent, false);
-        return new activity_workout_viewholder(cell_view);
+        View cell_view = inflater.inflate(R.layout.activity_schedule_routine_cell, parent, false);
+        return new activity_schedule_routine_viewholder(cell_view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         RoutineGroup group = routineGroups.get(position);
-        activity_workout_viewholder groupHolder = (activity_workout_viewholder) holder;
+        activity_schedule_routine_viewholder groupHolder = (activity_schedule_routine_viewholder) holder;
         groupHolder.showGroupData(group);
     }
 
